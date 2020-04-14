fd9716fc0649718adb1288ef93714aff90bd64d7
Kingsley <kingsleyumujeyan@gmail.com>
2020-04-14 19:46:36 +0100

    Updates

---
+++ precache-manifest.04bbf63de3eccac92e7c587ea27ce2ae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a2be6065792cb6b0ffedd63c24fefd4",
+    "url": "/Covid/index.html"
+  },
+  {
+    "revision": "52cb37024ea8e8f4bd74",
+    "url": "/Covid/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "2aad105e64b30e541e81",
+    "url": "/Covid/static/css/main.9a49773e.chunk.css"
+  },
+  {
+    "revision": "52cb37024ea8e8f4bd74",
+    "url": "/Covid/static/js/2.768be5e9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Covid/static/js/2.768be5e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2aad105e64b30e541e81",
+    "url": "/Covid/static/js/main.b6d38cfe.chunk.js"
+  },
+  {
+    "revision": "b4f275c5a3d267f521b3",
+    "url": "/Covid/static/js/runtime-main.d5e7da58.js"
+  }
+]);
