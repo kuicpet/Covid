aa5fef4f7fe7eb68c0656c6e1d83277e146d61e0
Kingsley <kingsleyumujeyan@gmail.com>
2020-04-15 00:32:18 +0100

    Updates

--- precache-manifest.f31ba72b63d34e2c91247f5ff2b7f1a3.js
+++ precache-manifest.f31ba72b63d34e2c91247f5ff2b7f1a3.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "905556970b261dfcea87cee97ea27078",
+    "revision": "78547e59ca06dfdb48bbf48130a2ec85",
     "url": "/Covid/index.html"
   },
   {
-    "revision": "bda7cc26a021bb273132",
-    "url": "/Covid/static/css/main.8a390da8.chunk.css"
+    "revision": "c1d082d538040be48bbe",
+    "url": "/Covid/static/css/main.86cd27f2.chunk.css"
   },
   {
     "revision": "5014bf72f826758190af",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Covid/static/js/2.b9275850.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "bda7cc26a021bb273132",
-    "url": "/Covid/static/js/main.32a0decd.chunk.js"
+    "revision": "c1d082d538040be48bbe",
+    "url": "/Covid/static/js/main.e1c674e5.chunk.js"
   },
   {
     "revision": "b4f275c5a3d267f521b3",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Covid/precache-manifest.05482e32e173b72e9fd33294208f2793.js"
+  "/Covid/precache-manifest.f31ba72b63d34e2c91247f5ff2b7f1a3.js"
 );
 
 self.addEventListener('message', (event) => {
